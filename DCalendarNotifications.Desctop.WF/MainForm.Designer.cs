f122effe6e0ff949e9a5144f647dbe794d10b7d8
DarkLord <dfhrhfan12@yandex.ru>
2024-10-16 21:21:59 +0400

    Upd main form

--- DCalendarNotifications.Desctop.WF/MainForm.Designer.cs
+++ DCalendarNotifications.Desctop.WF/MainForm.Designer.cs
@@ -33,7 +33,13 @@
             trayIcon = new System.Windows.Forms.NotifyIcon(components);
             trayIconMenuStrip = new System.Windows.Forms.ContextMenuStrip(components);
             закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            gbEvents = new System.Windows.Forms.GroupBox();
+            lbEvents = new System.Windows.Forms.ListBox();
+            gbLog = new System.Windows.Forms.GroupBox();
+            lbLog = new System.Windows.Forms.ListBox();
             trayIconMenuStrip.SuspendLayout();
+            gbEvents.SuspendLayout();
+            gbLog.SuspendLayout();
             SuspendLayout();
             // 
             // trayIcon
@@ -48,28 +54,77 @@
             // 
             trayIconMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { закрытьToolStripMenuItem });
             trayIconMenuStrip.Name = "trayIconMenuStrip";
-            trayIconMenuStrip.Size = new System.Drawing.Size(181, 48);
+            trayIconMenuStrip.Size = new System.Drawing.Size(121, 26);
             // 
             // закрытьToolStripMenuItem
             // 
             закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
-            закрытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            закрытьToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
             закрытьToolStripMenuItem.Text = "Закрыть";
             закрытьToolStripMenuItem.Click += trayIconMenuItem_close_Click;
+            // 
+            // gbEvents
+            // 
+            gbEvents.Controls.Add(lbEvents);
+            gbEvents.Dock = System.Windows.Forms.DockStyle.Top;
+            gbEvents.Location = new System.Drawing.Point(0, 0);
+            gbEvents.Name = "gbEvents";
+            gbEvents.Size = new System.Drawing.Size(480, 202);
+            gbEvents.TabIndex = 1;
+            gbEvents.TabStop = false;
+            gbEvents.Text = "События";
+            // 
+            // lbEvents
+            // 
+            lbEvents.Dock = System.Windows.Forms.DockStyle.Fill;
+            lbEvents.FormattingEnabled = true;
+            lbEvents.ItemHeight = 15;
+            lbEvents.Location = new System.Drawing.Point(3, 19);
+            lbEvents.Name = "lbEvents";
+            lbEvents.Size = new System.Drawing.Size(474, 180);
+            lbEvents.TabIndex = 0;
+            lbEvents.MouseDoubleClick += lbEvents_MouseDoubleClick;
+            // 
+            // gbLog
+            // 
+            gbLog.Controls.Add(lbLog);
+            gbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            gbLog.Location = new System.Drawing.Point(0, 202);
+            gbLog.Name = "gbLog";
+            gbLog.Size = new System.Drawing.Size(480, 155);
+            gbLog.TabIndex = 2;
+            gbLog.TabStop = false;
+            gbLog.Text = "Журнал";
+            // 
+            // lbLog
+            // 
+            lbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            lbLog.FormattingEnabled = true;
+            lbLog.ItemHeight = 15;
+            lbLog.Location = new System.Drawing.Point(3, 19);
+            lbLog.Name = "lbLog";
+            lbLog.Size = new System.Drawing.Size(474, 133);
+            lbLog.TabIndex = 1;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
+            ClientSize = new System.Drawing.Size(480, 357);
+            Controls.Add(gbLog);
+            Controls.Add(gbEvents);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "MainForm";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "MainForm";
+            TopMost = true;
             FormClosing += MainForm_FormClosing;
             trayIconMenuStrip.ResumeLayout(false);
+            gbEvents.ResumeLayout(false);
+            gbLog.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -78,5 +133,9 @@
         private System.Windows.Forms.NotifyIcon trayIcon;
         private System.Windows.Forms.ContextMenuStrip trayIconMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.GroupBox gbEvents;
+        private System.Windows.Forms.GroupBox gbLog;
+        private System.Windows.Forms.ListBox lbEvents;
+        private System.Windows.Forms.ListBox lbLog;
     }
 }
--- DCalendarNotifications.Desctop.WF/MainForm.cs
+++ DCalendarNotifications.Desctop.WF/MainForm.cs
@@ -1,3 +1,5 @@
+using DCalendarNotifications.Core;
+using System;
 using System.Windows.Forms;
 
 namespace DCalendarNotifications.Desctop.WF
@@ -7,7 +9,10 @@ namespace DCalendarNotifications.Desctop.WF
         public MainForm()
         {
             InitializeComponent();
+
         }
+
+        #region From Closing
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -24,6 +29,39 @@ namespace DCalendarNotifications.Desctop.WF
         private void trayIconMenuItem_close_Click(object sender, System.EventArgs e)
         {
             Application.Exit();
+        }
+
+        #endregion
+
+        private void lbEvents_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lbEvents.SelectedItem != null && lbEvents.SelectedItem is CalendarEvent calEvent)
+            {
+                ShowEventInfo(calEvent);
+            }
+        }
+
+        private void ShowEventInfo(CalendarEvent calEvent)
+        {
+            var eventForm = new EventForm(calEvent)
+            {
+                TopMost = true,
+                StartPosition = FormStartPosition.CenterScreen,
+            };
+
+            eventForm.Show();
+        }
+
+        private void AddLog(string message)
+        {
+            var logText = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]: {message}";
+            lbLog.Items.Add(logText);
+        }
+
+        private void AddLog(Exception ex)
+        {
+            var message = $"{ex.Message} Stack Trace: {ex.StackTrace}";
+            AddLog(message);
         }
     }
 }
